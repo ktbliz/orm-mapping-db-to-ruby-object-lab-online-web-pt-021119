009603d10ab68495937430db6eac09f421320a0c
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 04:02:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
+      SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     
     DB[:conn].execute(sql)
