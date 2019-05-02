d3edbf2eb280c2fce82af5ead159034438049515
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 04:16:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,12 +50,24 @@ class Student
     end 
   end 
   
-  def self.first_x_students_in_grade_10
+  def self.first_x_students_in_grade_10(x)
+     sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+      SQL
     
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end 
   end 
   
   def self.first_student_in_grade_10 
+     sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT  1
+      SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first 
   end 
   
   def self.all_students_in_grade_x 
