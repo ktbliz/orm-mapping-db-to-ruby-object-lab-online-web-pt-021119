77bd93100792ec5e9c4a3d3eb60a8d4daff5757d
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 03:56:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,10 @@ class Student
 
   def self.new_from_db(row)
     student = self.new 
-    student.id = 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
