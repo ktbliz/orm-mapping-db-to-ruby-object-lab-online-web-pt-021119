2ca978fc84dbaec4afa722de8010dc36767d9066
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 04:05:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,9 @@ class Student
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     
-    self.new_from_db(DB[:conn].execute(sql, name))
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
     
   end
   
