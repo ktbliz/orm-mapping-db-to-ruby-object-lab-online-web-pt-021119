973f89450f12c60a1d757ea25ac505c0f1279bbc
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 04:10:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,9 +33,12 @@ class Student
   
   
   def self.all_students_in_grade_9
-  
-  
-  
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+      SQL
+    
+    DB[:conn].execute(sql)
+  end
   
   
   def save
